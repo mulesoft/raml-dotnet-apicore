27b0ad2f62f02e5956fe24d290838fb4809d5dca
Adhikary, Nayan <nadhikary@cvent.com>
2018-10-26 09:55:39 -0400

    remove ms net http for public repo

--- src/RAML.API.Core/Properties/AssemblyInfo.cs
+++ src/RAML.API.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RAML.Api.Core")]
-[assembly: AssemblyDescription("Forked by Cvent on version 0.9.10 - Core API support classes for the RAML tools package")]
+[assembly: AssemblyDescription("Core API support classes for the RAML tools package")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MuleSoft Inc")]
 [assembly: AssemblyProduct("RAML.Api.Core")]
